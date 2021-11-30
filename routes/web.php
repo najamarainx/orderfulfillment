70ec59fbaeff9e9302eecbb0dd478d2750b44d87
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2021-11-30 14:22:07 +0500

    web file

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['middleware' => 'web'], function () {
 Route::get('/login', function () {
     return view('auth.login');
-})->middleware('auth');
-
-
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+}
+);
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
