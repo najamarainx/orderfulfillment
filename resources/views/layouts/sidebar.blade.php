b809747cb456466dad1f5ea28fee536763d91de0
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2021-12-22 15:43:26 +0500

    assembled lsiting

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -662,7 +662,7 @@
                                 </span>
                             </li>
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-                                <a href="javascript:;" class="menu-link menu-toggle">
+                                <a href="{{route('assembledOrderList')}}" class="menu-link menu-toggle">
                                     <i class="menu-bullet menu-bullet-line">
                                         <span></span>
                                     </i>
