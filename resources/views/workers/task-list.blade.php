7ec73f9c4a220db9ae14e04b084ba8df8c88ef0b
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2021-12-30 15:41:03 +0500

    tasklist

--- resources/views/workers/task-list.blade.php
+++ resources/views/workers/task-list.blade.php
@@ -597,7 +597,7 @@
         </div>
         <!--end::Container-->
     </div>
-
+    @include('layouts.common.quick_panel');
     <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
         <div class="d-flex flex-column-fluid">
             <!--begin::Container-->
