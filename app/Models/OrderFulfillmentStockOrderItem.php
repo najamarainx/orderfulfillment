4c758d49a65baf9551e26aeea7188d0af21c3c79
faraz511 <faraz@arainx.com>
2021-12-30 10:45:34 +0500

    add soft delete in stock order item

--- app/Models/OrderFulfillmentStockOrderItem.php
+++ app/Models/OrderFulfillmentStockOrderItem.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderFulfillmentStockOrderItem extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = "orderfulfillment_stock_order_items";
 
     public function orderItem()
