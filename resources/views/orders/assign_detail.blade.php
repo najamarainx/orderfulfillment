00c95f02ceec0a2fba9fa3afe52011b1854e6309
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2021-12-30 17:45:41 +0500

    asssign detail

--- resources/views/orders/assign_detail.blade.php
+++ resources/views/orders/assign_detail.blade.php
@@ -127,8 +127,7 @@
                                 @elseif(Auth::user()->type == 'installation' && Auth::user()->is_head == 0)
                                     <button type="button" id="update_assemble_stauts_btn" data-status="completed"
                                         data-id="{{ $orderItems->id }}"
-                                        class="btn btn-primary font-weight-bold update_assemble_stauts_btn">Proceed To
-                                        Inastallation</button>
+                                        class="btn btn-primary font-weight-bold update_assemble_stauts_btn">Proceed</button>
                                 @elseif(Auth::user()->type == 'super_admin' || Auth::user()->type == '')
                                     <button type="button" id="proceed_inventory" data-id="{{ $orderItems->id }}"
                                         class="btn btn-primary font-weight-bold">Proceed</button>
